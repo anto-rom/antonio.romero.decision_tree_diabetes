55ea6d5f3e1736f0ffef01b9a9ca29bb73296cae
anto-rom <anto-rom@hotmail.com>
2025-11-26 20:57:54 +0100

    Update Flask app for diabetes prediction
    Refactor app to use Flask for rendering and prediction.

--- src/app.py
+++ src/app.py
@@ -1,31 +1,38 @@
-# src/app.py
-from flask import Flask, request, jsonify
-import pickle
+from flask import Flask, request, render_template
+from pickle import load
+from sklearn.preprocessing import StandardScaler
 import numpy as np
-import os
-
-# Cargar el modelo entrenado
-MODEL_PATH = os.path.join("models", "modelo_arbol_diabetes.pkl")
-
-with open(MODEL_PATH, "rb") as f:
-    model = pickle.load(f)
+import pandas as pd
 
 app = Flask(__name__)
+model = load(open("decision_tree_classifier_default_42.sav", "rb"))
+class_dict = {
+    "0": "Sin diabetes",
+    "1": "Diabetes",
+}
+df = pd.read_csv("data.csv")
 
-@app.route("/")
-def health():
-    return "Service is up"
+num_variables = ['Pregnancies', 'Glucose', 'BloodPressure', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+scaler = StandardScaler()
+scaler.fit(df[num_variables])
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    """
-    Espera un JSON como:
-    {
-      "features": [valor1, valor2, ..., valorN]
-    }
-    """
-    data = request.get_json()
-    features = np.array(data["features"]).reshape(1, -1)
-    pred = int(model.predict(features)[0])
-    label = "diabetes" if pred == 1 else "no_diabetes"
-    return jsonify({"prediction": pred, "label": label})
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        Pregnancies = float(request.form["val1"])
+        Glucose = float(request.form["val2"])
+        BloodPressure = float(request.form["val3"])
+        BMI = float(request.form["val4"])
+        DiabetesPedigreeFunction = float(request.form["val5"])
+        Age = float(request.form["val6"])
+
+        data = np.array([[Pregnancies, Glucose, BloodPressure, BMI, DiabetesPedigreeFunction, Age]])
+
+        data_normalized = scaler.transform(data)
+
+        prediction = str(model.predict(data_normalized)[0])
+        pred_class = class_dict[prediction]
+    else:
+        pred_class = None
+    
+    return render_template("index.html", prediction=pred_class)
